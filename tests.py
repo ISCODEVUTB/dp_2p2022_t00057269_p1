2d783079556523322d199621a56280e97396d476
Santiago Ojeda <98894458+SantiTheDev@users.noreply.github.com>
2022-09-20 12:02:58 -0500

    rewriting tests.py file

--- tests.py
+++ tests.py
@@ -15,34 +15,34 @@ humanoRouge.set_sex("male")
 humanoRouge.set_heigth("6'5")
 humanoRouge.set_width("50")
 humanoRouge.set_description("feroz combatiente")
-humanoRouge.__str__()
-print("\n")
+
 
 #orc Test
 orcWarrior.set_sex("male")
 orcWarrior.set_heigth("7'8")
 orcWarrior.set_width("60")
 orcWarrior.set_description("combatiente que se las sabe todas")
-orcWarrior.__str__()
-print("\n")
+
 
 # dark elf test
 darkElfMage.set_sex("female")
 darkElfMage.set_heigth("5'7")
 darkElfMage.set_width("30")
 darkElfMage.set_description("hermosa maga")
-darkElfMage.__str__()
-print("\n")
+
 
 
 
 IFicha.defineEnemy(humanoRouge)
 IFicha.add(humanoRouge)
+print("\n")
 
 IFicha.defineEnemy(orcWarrior)
 IFicha.add(orcWarrior)
+print("\n")
 
 IFicha.defineEnemy(darkElfMage)
 IFicha.add(darkElfMage)
+print("\n")
 
 
